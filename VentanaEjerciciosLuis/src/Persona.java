60d884a1f8f3a2322531012f7bc0ac2dd403e795
AlejandroNieto-DAM <alejandrona5b@gmail.com>
2019-10-28 20:31:03 +0100

    funciona

---
+++ VentanaEjerciciosLuis/src/Persona.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alejandronieto
+ */
+public class Persona {
+    
+    private String nombre;
+    private Double altura;
+    private float peso;
+    private int edad;
+
+    public Persona(String nombre, Double altura, float peso, int edad) {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+}
